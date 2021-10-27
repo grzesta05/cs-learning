e6005b93f1a8c5b439d87a9ab4e4fd37e19379a6
grzesta05 <grzesta05@gmail.com>
2021-10-27 17:06:37 +0200

    Started working on eval function

--- 9 zadań początkowych/9zadań/9zadań/zadanie11.cs
+++ 9 zadań początkowych/9zadań/9zadań/zadanie11.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.JScript;
 
 namespace _9zadań
 {
-    class zadanie11
+    class zadanienr11
     {
+        double Kalkulator(double a, char s, double b)
+        {
+            var engine = VsaEngine.CreateEngine();
+            Eval.JScriptEvaluate(mySum, engine);
+        }
+        public static void z11()
+        {
+            
+        }
     }
 }
